330259da7d07797435eb5ff6ec540adef288a38a
emirxmertoglu <emirxmertoglu@outlook.com>
2021-02-07 14:38:23 +0300

    deleted helloworld, edited app, home, about and added jobs and jobdetails

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,31 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae, earum
+      laudantium. Ducimus velit similique possimus, aut distinctio reprehenderit
+      officiis amet quia, consequuntur quod cum sint repudiandae dolor dolore
+      esse natus hic impedit culpa explicabo dignissimos non asperiores
+      accusantium praesentium. Maxime, magnam corrupti veritatis atque
+      cupiditate amet quos, voluptatum delectus nisi deserunt nemo asperiores
+      aliquam adipisci rerum mollitia ratione at rem autem placeat esse facere
+      commodi. Consequatur ipsa nostrum recusandae blanditiis, eum in nam
+      voluptas odio voluptate, voluptatibus culpa quasi dicta delectus ratione
+      debitis ab! Fugit dolorum recusandae amet accusantium, nulla inventore nam
+      qui eum cum excepturi voluptas est exercitationem! Tempora?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae, earum
+      laudantium. Ducimus velit similique possimus, aut distinctio reprehenderit
+      officiis amet quia, consequuntur quod cum sint repudiandae dolor dolore
+      esse natus hic impedit culpa explicabo dignissimos non asperiores
+      accusantium praesentium. Maxime, magnam corrupti veritatis atque
+      cupiditate amet quos, voluptatum delectus nisi deserunt nemo asperiores
+      aliquam adipisci rerum mollitia ratione at rem autem placeat esse facere
+      commodi. Consequatur ipsa nostrum recusandae blanditiis, eum in nam
+      voluptas odio voluptate, voluptatibus culpa quasi dicta delectus ratione
+      debitis ab! Fugit dolorum recusandae amet accusantium, nulla inventore nam
+      qui eum cum excepturi voluptas est exercitationem! Tempora?
+    </p>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,40 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod error vitae
+      consequuntur ullam laborum? Facere aspernatur nesciunt sit eius, atque
+      consequuntur accusamus sunt iste quod? Eligendi, ipsum praesentium ad iure
+      inventore, obcaecati mollitia distinctio repellat, est accusantium
+      pariatur eum impedit aut nesciunt tempora ex delectus reiciendis? Sed
+      asperiores cumque doloribus? Quod dignissimos voluptatibus, libero
+      deleniti consectetur, odit necessitatibus enim minima dolores saepe,
+      laborum consequatur. Nisi eos temporibus reiciendis veritatis iusto
+      possimus atque perferendis nobis magni. Similique, dolorem ut aut ipsum
+      voluptates cum eaque. Excepturi explicabo quos labore eius esse laboriosam
+      sit deleniti qui nulla adipisci distinctio reiciendis, eveniet cupiditate
+      voluptas.
+    </p>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod error vitae
+      consequuntur ullam laborum? Facere aspernatur nesciunt sit eius, atque
+      consequuntur accusamus sunt iste quod? Eligendi, ipsum praesentium ad iure
+      inventore, obcaecati mollitia distinctio repellat, est accusantium
+      pariatur eum impedit aut nesciunt tempora ex delectus reiciendis? Sed
+      asperiores cumque doloribus? Quod dignissimos voluptatibus, libero
+      deleniti consectetur, odit necessitatibus enim minima dolores saepe,
+      laborum consequatur. Nisi eos temporibus reiciendis veritatis iusto
+      possimus atque perferendis nobis magni. Similique, dolorem ut aut ipsum
+      voluptates cum eaque. Excepturi explicabo quos labore eius esse laboriosam
+      sit deleniti qui nulla adipisci distinctio reiciendis, eveniet cupiditate
+      voluptas.
+    </p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  components: {},
+};
 </script>
